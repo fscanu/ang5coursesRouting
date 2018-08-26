456692671142f2f9518659d409dc49a5273a3118
fscanu <federico.scanu@gmail.com>
2018-08-26 10:20:37 +0200

    Restart Angular 5 course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,23 +13,7 @@ import {ServerComponent} from './servers/server/server.component';
 import {ServersService} from './servers/servers.service';
 import {RouterModule, Routes} from '@angular/router';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
-
-const appRoutes: Routes = [
-  {path: '', component: HomeComponent},
-  {
-    path: 'users', component: UsersComponent, children: [
-      {path: ':id/:name', component: UserComponent}
-    ]
-  },
-  {
-    path: 'servers', component: ServersComponent, children: [
-      {path: ':id/edit', component: EditServerComponent},
-      {path: ':id', component: ServerComponent}
-    ]
-  },
-  {path: 'not-found', component: PageNotFoundComponent},
-  {path: '**', redirectTo: '/not-found'}
-];
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -46,7 +30,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
